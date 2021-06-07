715651f73461523b5f26389beb7f7b156ed0a827
korinneramcharitar <77815286+korinneramcharitar@users.noreply.github.com>
2021-06-06 21:10:22 -0400

    Update App.java

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -1,3 +1,27 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Korinne Ramcharitar
+ */
+
+
+package base;
+
+
+package b
+import java.util.Scanner;
+
+public class App {
+public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("What is the input");
+    String name = input.nextLine();
+    System.out.println(name + " has " + name.length() + " characters.");
+}
+
+
+}
+
 package base;
 
 import java.util.Scanner;
